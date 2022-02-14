bcbe46e14bfa33bd30e73fe3d6d90ab225f1a5f6
Pavel <p.6a@tut.by>
2022-02-14 14:49:34 +0300

    feat: implement deleting employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react/cjs/react.production.min';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,28 +8,44 @@ import EmployeesAddFort from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name: 'Raffaello Santi', salary: 1200, increase: false, id: 1},
+                {name: 'Michelangelo Simoni', salary: 3000, increase: true, id: 2},
+                {name: 'Leonardo da Vinci', salary: 1700, increase: false, id: 3},
+                {name: 'Donato Bardi', salary: 4500, increase: false, id: 4}
+            ]
+        }
+    }
 
-    const data = [
-        {name: 'Raffaello Santi', salary: 1200, increase: false, id: 1},
-        {name: 'Michelangelo Simoni', salary: 3000, increase: true, id: 2},
-        {name: 'Leonardo da Vinci', salary: 1700, increase: false, id: 3},
-        {name: 'Donato Bardi', salary: 4500, increase: false, id: 4}
-    ]
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(e => e.id !== id)
+            }
+        })
+    }
 
-    return (
-        <div className="app">
-            <AppInfo/>
-
-            <div className='search-panel'>
-                <SearchPanel/>
-                <AppFilter/>
+    render() {
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className='search-panel'>
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddFort/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddFort/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
